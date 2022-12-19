5dfe1532d000192962f59c2eadd98e0bcf0dffb6
aamer Essa <aamer.es12@gmail.com>
2022-12-19 06:42:57 +0300

    Initial Commit

---
+++ Profile Card/Profile_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_CardApp.swift
+//  Profile Card
+//
+//  Created by Aamer Essa on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Profile_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
